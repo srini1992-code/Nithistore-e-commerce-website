7a641270505b1ad324273624c16797849babb3da
Srinidhi S <srinidhis1992dec@gmail.com>
2020-10-22 02:41:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+    var x = document.getElementById("menu_items");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
